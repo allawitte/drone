154a8b31435370a9ad66b54cadd003d713707c48
Alla Klimova <alla_88@inbox.lv>
2017-01-13 20:07:23 -0300

    ngMessages is added to mmodule app

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var lib = [
     'bower_components/angular-ui-router/release/angular-ui-router.js',
     'bower_components/angular-aria/angular-aria.js',
     'bower_components/angular-animate/angular-animate.js',
+    'bower_components/angular-messages/angular-messages.js',
     'bower_components/angular-material/angular-material.js'
 ];
 
