0cdee849b8c1826f4fbd6fb255bcad84566aef52
Alla Klimova <alla_88@inbox.lv>
2017-04-10 11:53:49 -0300

    io.socket doesn't work

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var User = require('./server/modules/user'); // get our mongoose model
-var Order = require('./server/modules/order'); // get our mongoose model
-const drone = require('netology-fake-drone-api');
 
 mongoose.Promise = require('bluebird');
 
@@ -32,70 +29,6 @@ app.use(morgan('dev'));
 var server = http.listen(app.get('port'), function () {
     console.log('Server starts on port 8000');
 });
-
-function updateStatus(updatedObject, status) {
-    updatedObject.status = status;
-    if(status == 4){
-        updatedObject.discount = true;
-    }
-    if(status > 2) {
-        updatedObject.time.push({status: status, moment: new Date()});
-    }
-    updatedObject.save(function (err, savedObject) {
-        if (err) {
-            console.log('err', err)
-        }
-        else {
-            console.log('savedObject', savedObject);
-            io.sockets.emit('order', savedObject);
-        }
-    });
-}
-function deliveryResult(updatedObject) {
-    console.log('updatedObject', updatedObject);
-
-    drone
-        .deliver()
-        .then(() => {
-            console.log('Доставлено');
-            updateStatus(updatedObject, 3);
-
-        })
-        .catch(() => {
-            console.log('Возникли сложности');
-            updateStatus(updatedObject, 4);
-        });
-}
-var orderChangeStatus =  function (req, res) {
-    var id = req.body._id;
-    var status = req.body.status;
-    var time = req.body.time;
-    if(status > 3) {
-        time.push({status: status, moment: new Date()});
-    }
-
-    Order.findOneAndUpdate({_id: id}, {
-        $set: {
-            "status": status,
-            "time": time
-        }
-    }, {new: true}, function (err, foundOrder) {
-        if (err) {
-            res.status(500).send('Error to update order: ' + id);
-        }
-        else {
-            if (foundOrder.status == 2) {
-                deliveryResult(foundOrder);
-            }
-            res.status(200).json({data: foundOrder});
-        }
-
-        io.sockets.emit('order', foundOrder);
-
-        console.log('updated', foundOrder);
-    });
-
-};
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
@@ -123,7 +56,7 @@ app.post('/auth', router.userAuth);
 
 app.post('/order', router.postOrder);
 
-app.put('/order/change-status', orderChangeStatus);
+app.put('/order/change-status', router.orderChangeStatus);
 
 app.get('/order/:clientId', router.getOrderByClientId);
 
